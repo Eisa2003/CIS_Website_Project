22b6071ae87110d65aac015db666073faa40216b
Eisa Chaudhary <eisa@email.uscb.edu>
2024-05-11 12:50:23 +0530

    Created and a working nav component using props and stuff

--- src/App.js
+++ src/App.js
@@ -1,39 +1,13 @@
 //import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
 
-function App() {
+function App() { 
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <a className="navbar-brand" href="/">CISSC</a>
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarNavDropdown">
-        <ul className="navbar-nav">
-          <li className="nav-item active">
-            <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="/">Contact Us</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="/">About</a>
-          </li>
-          <li className="nav-item dropdown">
-            <a className="nav-link dropdown-toggle" href="/" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              Resources
-            </a>
-            <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-              <a className="dropdown-item" href="/">Action</a>
-              <a className="dropdown-item" href="/">Another action</a>
-              <a className="dropdown-item" href="/">Something else here</a>
-            </div>
-          </li>
-        </ul>
-      </div>
-    </nav>
+    <Navbar title = "CISSC" /> // Dynamic component, I can change the title as 
+                               // I wish; possible due to props
   );
-}
+} // end function App
 
 
 export default App;
