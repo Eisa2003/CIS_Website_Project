611d66a126747d3a983278a0716c576524d8d657
unknown <eisa@email.uscb.edu>
2024-05-24 12:17:28 +0530

    Made some minor changes here and there - readjustments

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Events from './components/Events';
 import EventsCarousel from './components/EventsCarousel';
 import Alerts from './components/Alerts.jsx';
 import Carousel from './components/Carousel.jsx';
+import AboutUs from './components/AboutUs.jsx';
 
 
 function App() {
@@ -19,10 +20,13 @@ function App() {
           <Navbar title="HC" />         
           <Carousel title="Hampton County Resources"/> 
 
+          <br/>
+
+
           <div className="container">
           <div className="row">
             <div className="col">
-              <h1 className='text-center'>Upcoming Events</h1> <br/>
+              <h1 className='text-center'>Upcoming Events</h1>
               <hr/>
               <EventsCarousel events={Events}/>
             </div>
@@ -39,30 +43,10 @@ function App() {
       element: (
         <>
           <Navbar title="HC" />
-          <Carousel title=""/> 
-          <h1>About Us</h1>
-          <p> Thanks to funding from the Coastal Carolina Foundation, this online resource center was created by 
-            Communities In Schools of South Carolina (CISSC) in conjunction with the University of South Carolina Beaufort (USCB) 
-            to help the citizens of Hampton County identify and access needed services and resources.  This project was started by 
-            the Hampton County Coalition, 
-            and we would especially like to thank 
-            Trina Williams with the New Life Center for her many hours of work compiling these resources.
-            </p>
-            <br />
-            <p>We hope to continually update and improve this website and welcome your feedback as one way we will improve. 
-            If you have any questions, have any suggestions, or know of a resource that we are missing on our website, 
-            please contact helpinhamptoncounty@cisofsc.org.    
-            </p>
-            <br />
-
-            <p>CISSC began serving Hampton County School district during the 2019-2020 school 
-            year at what was then Wade Hampton High School and North District Middle School.  
-            As of the 2023-2024 school year, CISSC expanded to Estill Elementary School and Estill Middle School, 
-            and continued to serve both North District Middle School and the consolidated Hampton County High School.  
-            You can find out more about the CISSC program here:  www.cisofsc.org.   
-           </p>
-
-          <EventsCarousel events={Events}/>
+          <Carousel title="About Us"/>
+          <br/>
+          <AboutUs />
+          <br/>
           <Footer />
         </>
       ),
---
+++ src/components/AboutUs.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+export default function AboutUs() {
+  return (
+    <div className='container'>
+      <h1>About Us</h1>
+          <p> Thanks to funding from the Coastal Carolina Foundation, this online resource center was created by 
+            Communities In Schools of South Carolina (CISSC) in conjunction with the University of South Carolina Beaufort (USCB) 
+            to help the citizens of Hampton County identify and access needed services and resources.  This project was started by 
+            the Hampton County Coalition, 
+            and we would especially like to thank 
+            Trina Williams with the New Life Center for her many hours of work compiling these resources.
+            </p>
+            <br />
+            <p>We hope to continually update and improve this website and welcome your feedback as one way we will improve. 
+            If you have any questions, have any suggestions, or know of a resource that we are missing on our website, 
+            please contact <a href='mailto:helpinhamptoncounty@cisofsc.org'>helpinhamptoncounty@cisofsc.org.</a>  
+            </p>
+            <br />
+
+            <p>CISSC began serving Hampton County School district during the 2019-2020 school 
+            year at what was then Wade Hampton High School and North District Middle School.  
+            As of the 2023-2024 school year, CISSC expanded to Estill Elementary School and Estill Middle School, 
+            and continued to serve both North District Middle School and the consolidated Hampton County High School.  
+            You can find out more about the CISSC program here:<a href='www.cisofsc.org'>  www.cisofsc.org.</a>   
+           </p>
+    </div>
+  )
+}
